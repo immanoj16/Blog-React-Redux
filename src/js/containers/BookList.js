1ac694f62a340a79fe26e66d224ae102b288b667
immanoj16 <mmaharana14@gmail.com>
2017-08-19 05:35:36 +0530

    Add: action creators

--- src/js/containers/BookList.js
+++ src/js/containers/BookList.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { selectBook } from '../actions/index'
 
 class BookList extends Component {
 
   renderList () {
     return this.props.books.map((book) => {
       return (
-        <li className='list-group-item'>{book.title}</li>
+        <li onClick={() => this.props.selectBook(book)} className='list-group-item'>{book.title}</li>
       )
     })
   }
@@ -26,4 +29,11 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(BookList)
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    { selectBook: selectBook },
+    dispatch
+  )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
