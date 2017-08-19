7773eee52c928cb95322bbde02fb06b593ef7f4a
immanoj16 <mmaharana14@gmail.com>
2017-08-19 09:52:39 +0530

    Add: bookdetails reducer

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import BookList from './containers/BookList'
+import BookDetail from './containers/BookDetail'
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <div className='row'>
         <BookList/>
+        <BookDetail/>
       </div>
     )
   }
--- src/js/containers/BookList.js
+++ src/js/containers/BookList.js
@@ -9,16 +9,18 @@ class BookList extends Component {
   renderList () {
     return this.props.books.map((book) => {
       return (
-        <li onClick={() => this.props.selectBook(book)} className='list-group-item'>{book.title}</li>
+        <li onClick={() => this.props.selectBook(book)} key={book.title} className='list-group-item'>{book.title}</li>
       )
     })
   }
 
   render () {
     return (
-      <ul className='list-group col-sm-4'>
-        {this.renderList()}
-      </ul>
+      <div>
+        <ul className='list-group col-sm-4'>
+          {this.renderList()}
+        </ul>
+      </div>
     )
   }
 }
