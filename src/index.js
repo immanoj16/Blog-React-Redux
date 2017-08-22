571e1d4958619e8586a3fe013edea37d708ddf0b
immanoj16 <mmaharana14@gmail.com>
2017-08-22 12:22:21 +0530

    Add: fetchpost action creator to show individual post

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import promise from 'redux-promise'
 import reducers from './js/reducers';
 import PostsIndex from './js/components/PostsIndex'
 import PostsNew from './js/components/PostsNew'
+import PostsShow from './js/components/PostsShow'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -19,6 +20,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path='/posts/new' component={PostsNew} />
+          <Route path='/posts/:id' component={PostsShow} />
           <Route path='/' component={PostsIndex} />
         </Switch>
       </div>
